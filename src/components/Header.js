0109bf287ce9735088c072af68799f73fa0b182f
Vic <victordjohnson@gmail.com>
2018-12-30 18:33:19 -0600

    message

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,25 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <ul className="navbar">
-      <li className="nav-links">
+      <li>
         <NavLink exact={true} to="/">
           Home
         </NavLink>
       </li>
-      <li className="nav-links">
+      <li>
         <NavLink to="about">about</NavLink>
       </li>
-      <li className="nav-links">
+      <li>
         <NavLink to="about">Resume</NavLink>
       </li>
       <li className="name">Victor Johnson</li>
-      <li className="nav-links">
+      <li>
         <NavLink to="/simonsays">Simon Says</NavLink>
       </li>
-      <li className="nav-links">
+      <li>
         <NavLink to="/connect4">Connect4</NavLink>
       </li>
-      <li className="nav-links">
+      <li>
         <NavLink to="/spacebarrage">Space Barrage</NavLink>
       </li>
     </ul>
